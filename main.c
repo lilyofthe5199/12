9fbed7eb6332e54e03a643a3c653b071d234962e
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-11-19 16:16:08 +0900

    06

--- main.c
+++ main.c
@@ -3,29 +3,28 @@
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void) {
-	
-	FILE *fp;
+
+void main(void){
+
+	FILE *fp = NULL;
+	// char c;
 	char str[100];
-	int i;
 	
-	// "w" 쓰기 모드로 열기 
-	fp = fopen("C:\sample.txt", "w");
+	fp = fopen("sample.txt", "r");
+
 	if (fp == NULL)
 	{
 		return -1;
 	}
 	
-	for(i=0; i<3; i++)
-	{
-		printf("input a word:");
-		
-		scanf("%s", str);
-		
-		fprintf(fp, "%s\n", str);
+	
+	//while ( (c=fgetc(fp)) != EOF ){
+	while( fgets(str, 100, fp) != NULL){
+	
+		// printf("%c", c);
+		printf("%s", str);
 	}
 	
-	// 닫기 
 	fclose(fp);
 	
 	return 0;
