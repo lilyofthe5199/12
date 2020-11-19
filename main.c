4d36f34b11490862793c18d83f0e9ae1fd444e18
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-11-19 15:42:46 +0900

    03, 04

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-int main(void) {
-	
-	int i=0;
-	char str[4];
-	
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+// 실습 3, 실습 4 
 
-	printf("%s\n", str);
+void main(void) {
 	
-	// while (현재 문자 (str[i])이 맨 끝이 아님){
-	while (str[i] != str[3]){
-		// printf로 현재 문자 하나 출력;
-		printf("%c", str[i]);
-		// i를 1 증가;
-		i++; 
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	}
+	char str[30] = "happy C programming";
 	
-	return 0;
+	// strcpy() 함수 사용 (문자열 포인터를 인수로 넣음)
+	// <복사 대상 문자열> = strcpy ( <대상>, <원본> );
+	strcpy(dst, src);
+	
+	printf("copied string : %s\n", dst);
+	
+	// strlen() 함수 사용 (문자열 포인터를 인수로 넣음)
+	// <문자열 길이> = strlen ( <입력 문자열> );
+	printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
+	
+	
 }
